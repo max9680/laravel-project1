f94b7c557e2c8b55e957cd1590be107bc246ca1a
max9680@mail.ru <max9680@mail.ru>
2023-07-31 14:53:31 +0300

    build Chirper with Blade

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\MainController;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,13 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/create', [PostController::class, 'create']);
-Route::get('/posts/update', [PostController::class, 'update']);
-Route::get('/posts/delete', [PostController::class, 'delete']);
-Route::get('/posts/first_or_create', [PostController::class, 'firstOrCreate']);
-Route::get('/posts/update_or_create', [PostController::class, 'updateOrCreate']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/about', [AboutController::class, 'index'])->name('about.index');
-Route::get('/contacts', [ContactController::class, 'index'])->name('contact.index');
-Route::get('/main', [MainController::class, 'index'])->name('main.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store'])
+    ->middleware(['auth', 'verified']);
+
+require __DIR__.'/auth.php';
