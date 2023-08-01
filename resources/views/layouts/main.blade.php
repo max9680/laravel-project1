794d7e5ef760954c241894d9c8642df187591f23
m.vorobyev <max9680@mail.ru>
2023-08-01 17:32:02 +0300

    add header in main layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,18 +9,53 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<div class="container mx-auto ">
-    <div class="row">
-        <nav>
-            <ul>
-                <li><a href="{{route('main.index')}}">Main</a> </li>
-                <li><a href="{{route('posts.index')}}">Posts</a> </li>
-                <li><a href="{{route('about.index')}}">About</a> </li>
-                <li><a href="{{route('contact.index')}}">Contacts</a> </li>
-            </ul>
-        </nav>
-    </div>
+<body>
+
+
+<div id="app">
+<header class="fixed w-full">
+    <nav class="bg-white border-gray-200 py-2.5 dark:bg-gray-900 shadow-md">
+        <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
+            <a href="https://php-task-manager-ru.hexlet.app" class="flex items-center">
+                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Менеджер задач</span>
+            </a>
+
+            <div class="flex items-center lg:order-2">
+                <a href="https://php-task-manager-ru.hexlet.app/login" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Вход
+                </a>
+                <a href="https://php-task-manager-ru.hexlet.app/register" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
+                    Регистрация
+                </a>
+            </div>
+
+            <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
+                <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                    <li>
+                        <a href="{{route('main.index')}}" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                            Main                                </a>
+                    </li>
+                    <li>
+                        <a href="{{route('posts.index')}}" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                            Posts                              </a>
+                    </li>
+                    <li>
+                        <a href="{{route('about.index')}}" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                            About                                </a>
+                    </li>
+                    <a href="{{route('contact.index')}}" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">
+                        Contacts                                </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+<section class="bg-white dark:bg-gray-900">
+    <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
     @yield('content')
+    </div>
+</section>
 </div>
 </body>
 </html>
