5c8923771a6dfc80881ad3cbcad20dd67b82dda8
max9680@mail.ru <max9680@mail.ru>
2023-08-03 15:01:55 +0300

    add model category

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,45 +5,55 @@ namespace App\Http\Controllers;
 
 use App\Models\Dream;
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index() {
+    public function index()
+    {
 
-        $posts = Post::all();
+        $category = Category::find(1);
+        $post = Post::find(1);
 
-        return view('post.index', compact('posts'));
+        dd($category->posts);
+
+//        return view('post.index', compact('posts'));
     }
 
-    public function create() {
+    public function create()
+    {
 
         return view('post.create');
     }
 
-    public function store() {
+    public function store()
+    {
 
         $data = request()->validate([
-           'title' => 'string',
-           'content' => 'string',
-           'image' => 'string',
+            'title' => 'string',
+            'content' => 'string',
+            'image' => 'string',
         ]);
         Post::create($data);
 
         return redirect()->route('posts.index');
     }
 
-    public function show(Post $post) {
+    public function show(Post $post)
+    {
 
         return view('post.show', compact('post'));
     }
 
-    public function edit(Post $post) {
+    public function edit(Post $post)
+    {
 
         return view('post.edit', compact('post'));
     }
 
-    public function update(Post $post) {
+    public function update(Post $post)
+    {
 
         $data = request()->validate([
             'title' => 'string',
@@ -56,14 +66,16 @@ class PostController extends Controller
         return redirect()->route('posts.show', $post->id);
     }
 
-    public function destroy(Post $post) {
+    public function destroy(Post $post)
+    {
 
         $post->delete();
 
         return redirect()->route('posts.index');
     }
 
-    public function firstOrCreate() {
+    public function firstOrCreate()
+    {
 //        $post = Post::find(1);
 
         $anotherPost = [
@@ -76,13 +88,14 @@ class PostController extends Controller
 
         $post = Post::firstOrCreate([
             'title' => 'some title'
-            ], $anotherPost);
+        ], $anotherPost);
 
         dump($post->content);
         dd('finished');
     }
 
-    public function updateOrCreate() {
+    public function updateOrCreate()
+    {
         $anotherPost = [
             'title' => 'some title',
             'content' => 'some new content',
@@ -93,7 +106,7 @@ class PostController extends Controller
 
         $post = Post::updateOrCreate([
             'title' => 'some new title'
-            ], $anotherPost);
+        ], $anotherPost);
 
         dump($post->content);
         dd('finished');
