38ce8a581188e08cdb007a331a4be6f3e80c4a74
max9680@mail.ru <max9680@mail.ru>
2023-08-25 16:08:04 +0300

    add factory and seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Category::factory(3)->create();
+        $tags = Tag::factory(5)->create();
+        $posts = Post::factory(10)->create();
+
+        foreach ($posts as $post) {
+            $tagsIds = $tags->random(2)->pluck('id');
+            $post->tags()->attach($tagsIds);
+        }
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
